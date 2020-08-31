499e294f32b9a1503c925e51bfc1941145cb8e6f
asef18766 <asef18766@gmail.com>
2020-08-31 22:22:26 +0800

    add lock

--- sticky_note/handler.py
+++ sticky_note/handler.py
@@ -1,23 +1,34 @@
 import logging
+from threading import Lock
 rem_item = []
+lock = Lock()
 def add_rem(item:str):
     global rem_item
+    global lock
+    lock.acquire(blocking=True)
     rem_item.append(item)
+    lock.release()
 
 def remove_rem(idx:int)->bool:
     global rem_item
-    logging.debug(f"try to delete item {idx}")
     if len(rem_item) < idx:
         return False
-    rem_item.pop(index=idx)
+    global lock
+    lock.acquire(blocking=True)
+    rem_item.pop(idx)
+    lock.release
     return True
 
 def list_rem()->str:
     global rem_item
+    global lock
+    lock.acquire(blocking=True)
     if  len(rem_item) == 0:
+        lock.release()
         return "沒東西啦 臭ㄐㄐ"
     else:
         s = "剩下這些(OuO):\n"
         for i in range(len(rem_item)):
             s += f"{i}:{rem_item[i]}\n"
+        lock.release()
         return s
