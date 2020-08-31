ab3ab6dee1dc848d1ddc59158ec314bf8af5a025
asef18766 <asef18766@gmail.com>
2020-08-31 23:34:53 +0800

    add db debug

--- database/handler.py
+++ database/handler.py
@@ -1,6 +1,9 @@
 import os
 import psycopg2
+import logging
+
 DATABASE_URL = os.popen('heroku config:get DATABASE_URL -a feizao-bot').read()[:-1]
+logging.info(f"db url:{DATABASE_URL}")
 conn:psycopg2.extensions.connection = psycopg2.connect(DATABASE_URL, sslmode='require')
 cursor:psycopg2.extensions.cursor = conn.cursor()
 def init():
