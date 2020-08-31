30ad80126f655c064ea3cbe36d3dad78f83e7599
asef18766 <asef18766@gmail.com>
2020-09-01 00:11:51 +0800

    test

--- database/handler.py
+++ database/handler.py
@@ -7,6 +7,8 @@ logging.info(f"db url:{DATABASE_URL}")
 conn:psycopg2.extensions.connection = psycopg2.connect(DATABASE_URL, sslmode='require')
 cursor:psycopg2.extensions.cursor = conn.cursor()
 def init():
+    global conn
+    global cursor
     cmd = '''
     CREATE TABLE sticky_note(
         id serial PRIMARY KEY,
