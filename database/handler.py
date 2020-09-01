a561bc9264bbf54d65ca1087497ac3c9da34eb89
asef18766 <asef18766@gmail.com>
2020-09-02 00:49:20 +0800

    test

--- database/handler.py
+++ database/handler.py
@@ -5,11 +5,13 @@ DATABASE_URL = os.getenv("DATABASE_URL" , "error")
 conn:psycopg2.extensions.connection = psycopg2.connect(DATABASE_URL, sslmode='require')
 cursor:psycopg2.extensions.cursor = conn.cursor()
 logging.basicConfig(level=logging.DEBUG)
-
+inited = False
 def init():
     logging.info(f"db url:{DATABASE_URL}")
     global conn
     global cursor
+    global inited
+    inited = True
     cmd = '''
     CREATE TABLE sticky_note(
         id serial PRIMARY KEY,
