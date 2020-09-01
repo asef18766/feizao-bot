9cb1a919c2cb2737899e76bd6850c6c4b1c68448
asef18766 <asef18766@gmail.com>
2020-09-02 00:36:13 +0800

    test

--- database/handler.py
+++ database/handler.py
@@ -1,12 +1,11 @@
 import os
 import psycopg2
 import logging
-
 DATABASE_URL = os.getenv("DATABASE_URL" , "error")
-logging.info(f"db url:{DATABASE_URL}")
 conn:psycopg2.extensions.connection = psycopg2.connect(DATABASE_URL, sslmode='require')
 cursor:psycopg2.extensions.cursor = conn.cursor()
 def init():
+    logging.info(f"db url:{DATABASE_URL}")
     global conn
     global cursor
     cmd = '''
