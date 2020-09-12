1947d7069c5bf4bc8a46e59c365ce1e55ffa7cc1
bogay <pojay11523@gmail.com>
2020-09-12 16:31:07 +0800

    Make sticky note handler more pythonic

--- sticky_note/handler.py
+++ sticky_note/handler.py
@@ -1,28 +1,32 @@
 import logging
 from database.handler import cursor
-def add_rem(item:str):
+
+
+def add_rem(item: str):
     logging.info(f"try to insert item {item}")
-    cmd = '''
+    cmd = """
     INSERT INTO sticky_note (ctx) VALUES (%s);
-    '''
-    cursor.execute(cmd,(item,))
+    """
+    cursor.execute(cmd, (item, ))
 
-def remove_rem(idx:int)->bool:
-    cmd = '''
+
+def remove_rem(idx: int) -> bool:
+    cmd = """
     DELETE FROM sticky_note WHERE id = (%s);
-    '''
-    cursor.execute(cmd,(idx,))
+    """
+    cursor.execute(cmd, (idx, ))
 
-def list_rem()->str:
-    cmd = '''
+
+def list_rem() -> str:
+    cmd = """
     SELECT ctx FROM sticky_note;
-    '''
+    """
     cursor.execute(cmd)
-    rem_item = cursor.fetchall()
-    if  len(rem_item) == 0:
+    rem_items = cursor.fetchall()
+    if len(rem_items) == 0:
         return "沒東西啦 臭ㄐㄐ"
     else:
         s = "剩下這些(OuO):\n"
-        for i in range(len(rem_item)):
-            s += f"{i}:{rem_item[i][0]}\n"
+        for i, item in enumerate(rem_items):
+            s += f"{i}: {item[0]}\n"
         return s
